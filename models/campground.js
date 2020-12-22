3640bd38f709bb92b5d5ecfc8af2dbf3c1b94852
Radhika <radhika.makkar@impactqa.com>
2020-12-22 18:13:23 +0530

    first commit

---
+++ models/campground.js
@@ -0,0 +1,32 @@
+const mongoose=require('mongoose');
+const { campgroundSchema } = require('../schemas');
+const review = require('./review');
+const Schema=mongoose.Schema;
+
+const CampGroundSchema=new Schema({
+    title:String,
+    image:String,
+    price:Number,
+    description:String,
+    location:String,
+    reviews:[
+        {
+            type:Schema.Types.ObjectId,
+            ref:'Review'
+        }
+    ]
+})
+CampGroundSchema.post('findOneAndDelete',async function(doc)
+{
+if(doc) 
+{
+    await review.deleteOne({
+        _id:
+        {
+            $in:doc.reviews
+        }
+    })
+} 
+})
+module.exports=mongoose.model('Campground',CampGroundSchema);
+
